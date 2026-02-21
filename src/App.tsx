fea5706649af4bcad7101473a9cc7bde45f33e1f
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2026-02-22 00:08:49 +0630

    add: routing file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
+import { Home } from "@/pages/home/Home";
+import { Order } from "@/pages/order/Order";
+import { Checkout } from "@/pages/checkout/Checkout";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center"></div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/order" element={<Order />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/order/Order.tsx
@@ -0,0 +1,7 @@
+export function Order() {
+  return (
+    <div>
+      <p>This is order page</p>
+    </div>
+  );
+}
